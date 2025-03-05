d28ea9a3190a72c53789c66efb2a99e5cbeac4a5
Khoalt0811 <22162017@student.hcmute.edu.vn>
2025-03-05 15:23:51 +0700

    Update lekhoa.c

--- lekhoa.c
+++ lekhoa.c
@@ -1,20 +1,37 @@
+/**
+ * HTTP Server với thông tin hệ thống
+ * Tác giả: Khoalt0811
+ * Ngày: 2025-03-05 08:19:17
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <sys/utsname.h>
 #include <sys/sysinfo.h>
 #include <time.h>
 
-void http_response(int client_socket, int connection_count) {
+#define PORT 8080
+#define SERVER_IP "192.168.119.120"
+#define BUFFER_SIZE 4096
+
+// Biến toàn cục để đếm số kết nối
+int connection_count = 0;
+
+void http_response(int client_socket) {
     char hostname[256];
     struct utsname system_info;
     struct sysinfo sys_info;
-    char html_content[4096];
+    char html_content[BUFFER_SIZE];
     char uptime_str[64];
     time_t current_time;
     struct tm *time_info;
     char time_str[64];
+    
+    // Tăng số kết nối
+    connection_count++;
     
     // Lấy hostname
     gethostname(hostname, sizeof(hostname));
@@ -66,7 +83,7 @@ void http_response(int client_socket, int connection_count) {
         "    <li><strong>Hệ thống:</strong> %s %s</li>\r\n"
         "    <li><strong>Kiến trúc CPU:</strong> %s</li>\r\n"
         "    <li><strong>Kernel:</strong> %s</li>\r\n"
-        "    <li><strong>Địa chỉ IP:</strong> 192.168.119.120</li>\r\n"
+        "    <li><strong>Địa chỉ IP:</strong> %s</li>\r\n"
         "  </ul>\r\n"
         "  <h2>Thông tin người dùng</h2>\r\n"
         "  <ul>\r\n"
@@ -86,6 +103,7 @@ void http_response(int client_socket, int connection_count) {
         system_info.sysname, system_info.release, 
         system_info.machine, 
         system_info.version,
+        SERVER_IP,
         time_str
     );
     
@@ -106,4 +124,76 @@ void http_response(int client_socket, int connection_count) {
     
     // Gửi nội dung
     write(client_socket, html_content, strlen(html_content));
+}
+
+int main() {
+    int server_fd, client_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    
+    // Tạo socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Thiết lập socket options
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Thiết lập địa chỉ server
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(SERVER_IP);
+    address.sin_port = htons(PORT);
+    
+    // Bind socket vào địa chỉ và port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Listen cho kết nối đến
+    if (listen(server_fd, 10) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("HTTP Server đang chạy tại http://%s:%d\n", SERVER_IP, PORT);
+    printf("Người dùng: Khoalt0811\n");
+    printf("Thời gian hiện tại (UTC): 2025-03-05 08:19:17\n");
+    printf("Nhấn Ctrl+C để dừng server.\n");
+    
+    // Vòng lặp xử lý các kết nối
+    while(1) {
+        printf("Đang đợi kết nối...\n");
+        
+        // Chấp nhận kết nối đến
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("Accept failed");
+            continue;
+        }
+        
+        printf("Kết nối từ %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+        
+        // Xử lý kết nối
+        char buffer[BUFFER_SIZE] = {0};
+        int bytes_read = read(client_socket, buffer, BUFFER_SIZE);
+        if (bytes_read > 0) {
+            printf("Request nhận được:\n%.100s...\n", buffer);
+        }
+        
+        // Gửi response
+        http_response(client_socket);
+        
+        // Đóng kết nối
+        close(client_socket);
+        printf("Đóng kết nối. Tổng số kết nối đã xử lý: %d\n", connection_count);
+    }
+    
+    // Đóng server socket
+    close(server_fd);
+    return 0;
 }
